deca1b3c4738efd895644240344e06afd6225f44
rikiniki <ristevskin@gmail.com>
2026-07-29 00:35:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,16 @@ from database import inicijalizuj_bazu
 inicijalizuj_bazu()
 
 print("Baza uspešno kreirana")
+import sqlite3
+
+conn = sqlite3.connect("salon.db")
+c = conn.cursor()
+
+c.execute("""
+SELECT name FROM sqlite_master 
+WHERE type='table'
+""")
+
+print(c.fetchall())
+
+conn.close()
